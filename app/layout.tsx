b29fe28ce1f0a48b769f80c211808a4eab8b0bde
Smiths935 <yvanfotso935@gmail.com>
2025-08-31 19:21:30 +0100

    updated widget

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,8 +47,8 @@ export default function RootLayout({
             <Navigation />
             <PageTransition>
               {children}
-              <Widget />
             </PageTransition>
+              <Widget />
             <Toaster />
           </LanguageProvider>
         </ThemeProvider>
